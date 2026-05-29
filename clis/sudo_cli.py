fe86b0ae4e778ddf95fd280a0c0f9ab62543805b
DomeniGeco <domenigeco@gmail.com>
2026-05-29 21:28:40 +0200

    Create sudo_cli.py

---
+++ clis/sudo_cli.py
@@ -0,0 +1,12 @@
+"""
+Attenzione:
+
+Per usarlo è necessario compilarlo con admin:
+
+Windows: pyinstaller --onefile --uac-admin sudo_cli.py
+Linux: pyinstaller --onefile sudo_cli.py
+       sudo sudo_cli
+
+"""
+
+print("Sudo CLI pin sviluppo.")
