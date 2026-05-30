ba1de0ec51a5bf96ee3b234134e86968d8c0ba25
DomeniGeco <domenigeco@gmail.com>
2026-05-30 08:24:25 +0200

    Update sudo_cli.py

--- clis/sudo_cli.py
+++ clis/sudo_cli.py
@@ -9,4 +9,38 @@ Linux: pyinstaller --onefile sudo_cli.py
 
 """
 
-print("Sudo CLI pin sviluppo.")
+import sys
+import subprocess
+from pathlib import Path
+import os
+
+ROOT_DIR = Path(__file__).parent.parent
+
+if len(sys.argv) > 1 and sys.argv[1] == "compile":
+    subprocess.run(
+        [sys.executable, "-m", "pip", "install", "pyinstaller"],
+        check=True
+    )
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "PyInstaller",
+            "--onefile",
+            "--hidden-import=pyos_gaps",
+            str(ROOT_DIR / "main.py"),
+            "--name=pythonos",
+        ],
+        check=True
+    )
+
+    print("Compilazione completata! Si trova in:", end=" ")
+
+    if os.name == "nt":
+        print(ROOT_DIR / "dist" / "pythonos.exe")
+    else:
+        print(ROOT_DIR / "dist" / "pythonos")
+
+else:
+    print("SUDO CLI PythonOS")
